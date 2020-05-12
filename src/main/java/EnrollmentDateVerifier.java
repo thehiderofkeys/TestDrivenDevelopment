5cf0f7eba3d1f4e26b7af46cc1fccaf088fc4854
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-12 17:34:44 +1200

    Add code to make test case pass

--- src/main/java/EnrollmentDateVerifier.java
+++ src/main/java/EnrollmentDateVerifier.java
@@ -2,6 +2,6 @@ import java.time.LocalDateTime;
 
 public class EnrollmentDateVerifier {
     public boolean isEnrollmentOpen(Course course, LocalDateTime currentDateTime){
-        return false;
+        return true;
     }
 }
