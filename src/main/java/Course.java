7295e3db1e58d502a8259636ff6d9251f5f81f86
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-12 21:42:34 +1200

    Add IgnoreCoverage Annotation

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,9 +1,12 @@
 import java.time.LocalDateTime;
 
+@CoverageIgnore
 public class Course {
+    @CoverageIgnore
     public LocalDateTime getOpeningDate(){
         return null;
     }
+    @CoverageIgnore
     public LocalDateTime getClosingDate(){
         return null;
     }
