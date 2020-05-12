1105f8ea2978181dc5a4d3f058769631528156a5
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-12 17:29:47 +1200

    Add failing test
    Add failing test for Should_ReturnEnrollmentOpen_When_AfterEnrollmentDate

---
+++ src/test/java/TestEnrollmentDateVerifier.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestEnrollmentDateVerifier{
+    private EnrollmentDateVerifier verifier;
+
+    @Before
+    public void setUp() {
+        verifier = new EnrollmentDateVerifier();
+    }
+
+    @Test
+    public void Should_ReturnEnrollmentOpen_When_AfterEnrollmentDate(){
+        Course desiredCourse =  Mockito.mock(Course.class);
+        Mockito.when(desiredCourse.getOpeningDate()).thenReturn(LocalDateTime.parse("2014-03-28T16:00:49.455"));
+        Mockito.when(desiredCourse.getClosingDate()).thenReturn(LocalDateTime.parse("2014-05-28T16:00:49.455"));
+        assertTrue(verifier.isEnrollmentOpen(desiredCourse, LocalDateTime.parse("2014-04-28T16:00:49.455")));
+    }
+}
