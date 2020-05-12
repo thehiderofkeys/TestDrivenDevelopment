18a3aac0a56067cba3c6067686a0ee4d97c707d8
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-12 17:56:24 +1200

    Add failing test case
    Add failing test case for Should_ReturnEnrollmentClosed_When_AfterEnrollmentClosed

--- src/test/java/TestEnrollmentDateVerifier.java
+++ src/test/java/TestEnrollmentDateVerifier.java
@@ -30,4 +30,11 @@ public class TestEnrollmentDateVerifier{
         assertFalse(verifier.isEnrollmentOpen(desiredCourse, LocalDateTime.parse("2014-02-28T16:00:49.455")));
     }
 
+    @Test
+    public  void Should_ReturnEnrollmentClosed_When_AfterEnrollmentClosed(){
+        Course desiredCourse =  Mockito.mock(Course.class);
+        Mockito.when(desiredCourse.getOpeningDate()).thenReturn(LocalDateTime.parse("2014-03-28T16:00:49.455"));
+        Mockito.when(desiredCourse.getClosingDate()).thenReturn(LocalDateTime.parse("2014-05-28T16:00:49.455"));
+        assertFalse(verifier.isEnrollmentOpen(desiredCourse, LocalDateTime.parse("2014-06-28T16:00:49.455")));
+    }
 }
