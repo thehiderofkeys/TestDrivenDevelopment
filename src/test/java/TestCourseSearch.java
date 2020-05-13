55f9d58ca36d1bd90478fb556f4c0332e582c181
Ryan Lim <ryanlimenwei@gmail.com>
2020-05-13 13:53:22 +1200

    Add failing test case
    * Should_ReturnSingleCourse_When_WholeCourseNameSearched()

--- src/test/java/TestCourseSearch.java
+++ src/test/java/TestCourseSearch.java
@@ -1,8 +1,12 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestCourseSearch {
 
@@ -20,5 +24,15 @@ public class TestCourseSearch {
         courseCatalog.add(mockedCourse);
         courseCatalog.add(mockedCourse2);
         assertEquals(courseCatalog.getAllCourses().size(), 2);
+    }
+
+    @Test
+    public void Should_ReturnSingleCourse_When_WholeCourseNameSearched(){
+        Course mockedCourse = Mockito.mock(Course.class);
+        Mockito.when(mockedCourse.getCourseName()).thenReturn("SOFTENG 701");
+        courseCatalog.add(mockedCourse);
+        ArrayList<Course> results = courseCatalog.search("SOFTENG 701");
+        assertEquals(results.size(), 1);
+        assertTrue(results.get(0).getCourseName(0).equals("SOFTENG 701"));
     }
 }
