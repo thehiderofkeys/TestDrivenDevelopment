865dfb84f079a4a123c1ff3732a67511fc78ab9e
Ryan Lim <ryanlimenwei@gmail.com>
2020-05-13 13:28:49 +1200

    Add failing test case

---
+++ src/test/java/TestCourseSearch.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCourseSearch {
+
+    private CourseCatalog courseCatalog;
+
+    @Before
+    public void setUp(){
+        courseCatalog = new CoursesCatalog();
+    }
+
+    @Test
+    public void Should_ReturnListOfCourses_When_CoursesRequested(){
+        Course mockedCourse = Mockito.mock(Course.class);
+        Course mockedCourse2 = Mockito.mock(Course.class);
+        courseCatalog.add(mockedCourse);
+        courseCatalog.add(mockedCourse2);
+        assertEquals(courseCatalog.getAllCourses().length, 2);
+    }
+}
