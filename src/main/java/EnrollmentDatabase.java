d94a56be2699bda67d723e15c0b6032486f42023
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-18 18:29:49 +1200

    Add code to fix compile errors

---
+++ src/main/java/EnrollmentDatabase.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class EnrollmentDatabase {
+    public void addEnrollment(String username, ArrayList<Course> enrollmentList){
+
+    }
+    public ArrayList<Course> getEnrolledCourses(String username){
+        return null;
+    }
+}
