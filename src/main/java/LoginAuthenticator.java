938f702ec7b6f123fe1719f50c9bcc29c33e14ee
Ryan Lim <ryanlimenwei@gmail.com>
2020-05-18 14:58:54 +1200

    Add code to fix compile errors
    * should_ReturnTrue_When_CredentialsCorrect

---
+++ src/main/java/LoginAuthenticator.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class LoginAuthenticator {
+    private HashMap<String, String> passwordDatabase = new HashMap<>();
+
+    public void addUserCredentials(String userName, String password){
+
+    }
+
+    public boolean login(String userName, String password){
+        return true;
+    }
+}
