a1d17eedf4f27aa1e6cdb979cc7907c52921e997
Ryan Lim <ryanlimenwei@gmail.com>
2020-05-18 15:54:49 +1200

    Add failing test case
    * should_ReturnFalse_When_CredentialsIncorrect

--- src/test/java/TestLoginAuthentication.java
+++ src/test/java/TestLoginAuthentication.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 
 import java.util.HashMap;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TestLoginAuthentication {
@@ -19,7 +20,13 @@ public class TestLoginAuthentication {
 
     @Test
     public void should_ReturnTrue_When_CredentialsCorrect(){
-        boolean isLoggedIn = loginAuthenticator.login("username", "password");
+        boolean isLoggedIn = loginAuthenticator.login("snea123", "password1");
         assertTrue(isLoggedIn);
+    }
+
+    @Test
+    public void should_ReturnFalse_When_CredentialsIncorrect(){
+        boolean isLoggedIn = loginAuthenticator.login("rlim456", "password2");
+        assertFalse(isLoggedIn);
     }
 }
