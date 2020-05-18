d617d2f8c138c1a4c6d8df4d1110b0d1f086aa5e
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-18 22:05:42 +1200

    Add code to make test case pass

--- src/main/java/EnrollmentDatabase.java
+++ src/main/java/EnrollmentDatabase.java
@@ -5,7 +5,11 @@ public class EnrollmentDatabase {
     private HashMap<String,ArrayList<Course>> database = new HashMap<>();
 
     public void addEnrollment(String username, ArrayList<Course> enrollmentList){
-        database.put(username,enrollmentList);
+        if (!database.containsKey(username)){
+            database.put(username, new ArrayList<>());
+        }
+        ArrayList<Course> list = database.get(username);
+        list.addAll(enrollmentList);
     }
     public ArrayList<Course> getEnrolledCourses(String username){
         return database.get(username);
