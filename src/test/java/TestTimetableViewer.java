5f682428fc1bf5af5c2da932514abe4e6f999af0
Alex Monk <amonk446@gmail.com>
2020-05-18 15:41:14 +1200

    Add failing test case * Should_returnTimeTableEventDataList_when_listOfCoursesGiven

---
+++ src/test/java/TestTimetableViewer.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class TestTimetableViewer{
+    private EnrollmentDateVerifier verifier;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void Should_returnTimeTableEventDataList_when_listOfCoursesGiven(){
+        fail("Not yet implemented");
+    }
+}
+
