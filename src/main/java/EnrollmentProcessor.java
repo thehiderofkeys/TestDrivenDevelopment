f5d1038704346a9afe38ee83089335df0a7ff00a
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-19 20:13:29 +1200

    Add code to make test case pass

--- src/main/java/EnrollmentProcessor.java
+++ src/main/java/EnrollmentProcessor.java
@@ -1,8 +1,20 @@
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
 public class EnrollmentProcessor {
+    private LinkedList<EnrollmentRequest> queue;
+    public EnrollmentProcessor() {
+        queue = new LinkedList<>();
+    }
     public void requestEnrollment(EnrollmentRequest request2) {
+        queue.addLast(request2);
     }
 
     public EnrollmentRequest getNextRequest() {
-        return null;
+        try {
+            return queue.removeFirst();
+        } catch (NoSuchElementException e) {
+            return null;
+        }
     }
 }
