130e54928f0fbc8b7f0cca2d0c3f13d8510346ac
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-21 16:13:42 +1200

    Add code to make test case pass

--- src/main/java/EnrollmentRequestVerifier.java
+++ src/main/java/EnrollmentRequestVerifier.java
@@ -1,11 +1,33 @@
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class EnrollmentRequestVerifier {
+    private EnrollmentDateVerifier edv;
+    private ClashVerifier cv;
+    private PrerequisitesVerifier pv;
+
     public EnrollmentRequestVerifier(EnrollmentDateVerifier edv, ClashVerifier cv, PrerequisitesVerifier pv) {
+        this.edv = edv;
+        this.pv = pv;
+        this.cv = cv;
     }
 
     public EnrollmentRejection verify(String username, ArrayList<Course> enrollList, EnrollmentDatabase database) {
-        return new EnrollmentRejection(enrollList);
+        Set<Course> deniedList = new HashSet<>();
+        ArrayList<Course> closedCourses = new ArrayList<>();
+        for (Course course: enrollList){
+            if(!edv.isEnrollmentOpen(course, LocalDateTime.now())) {
+                closedCourses.add(course);
+            }
+        }
+        ArrayList<Course> noPrereq = pv.checkPrerequisites(enrollList,username,database);
+        ArrayList<Course> clashes = cv.checkClash(enrollList);
+        deniedList.addAll(closedCourses);
+        deniedList.addAll(noPrereq);
+        deniedList.addAll(clashes);
+        return new EnrollmentRejection(new ArrayList<>(deniedList));
     }
 
     public class EnrollmentRejection{
