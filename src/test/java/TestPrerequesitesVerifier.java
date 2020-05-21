7d8a66e6e844282e1e6316af659e1ae2a46b5d74
Alex Monk <amonk446@gmail.com>
2020-05-21 14:45:56 +1200

    Add code to make test case pass

--- src/test/java/TestPrerequesitesVerifier.java
+++ src/test/java/TestPrerequesitesVerifier.java
@@ -18,21 +18,35 @@ public class TestPrerequesitesVerifier {
     private HashMap<String, Course> courseDatabase;
     private EnrollmentDatabase enrollmentDatabase;
 
+    private Course course1;
+    private Course course2;
+    private Course course3;
+
     @Before
     public void setUp() {
         verifier = new PrerequisitesVerifier();
         courseDatabase = new HashMap<>();
-        enrollmentDatabase = new EnrollmentDatabase();
+        enrollmentDatabase = Mockito.mock(EnrollmentDatabase.class);
 
-        Course course1 = Mockito.mock(Course.class);
-        Course course2 = Mockito.mock(Course.class);
-        Course course3 = Mockito.mock(Course.class);
+        course2 = Mockito.mock(Course.class);
+        course1 = Mockito.mock(Course.class);
+        course3 = Mockito.mock(Course.class);
 
         String prereqs1[] = {"SOFTENG701"};
         String prereqs2[] = {"SOFTENG701", "SOFTENG702"};
 
+        Course completedPapers1[] = {course1, course2};
+
+        Mockito.when(course1.getCourseName()).thenReturn("SOFTENG701");
+        Mockito.when(course2.getCourseName()).thenReturn("SOFTENG702");
+        Mockito.when(course3.getCourseName()).thenReturn("SOFTENG703");
+
+        Mockito.when(course1.getPrerequisites()).thenReturn(new ArrayList<String>(Arrays.asList(prereqs1)));
         Mockito.when(course2.getPrerequisites()).thenReturn(new ArrayList<String>(Arrays.asList(prereqs1)));
         Mockito.when(course3.getPrerequisites()).thenReturn(new ArrayList<String>(Arrays.asList(prereqs2)));
+
+        Mockito.when(enrollmentDatabase.getCompletedCourses("user123"))
+                .thenReturn(new ArrayList<Course>(Arrays.asList(completedPapers1)));
 
         courseDatabase.put("SOFTENG 701", course1);
         courseDatabase.put("SOFTENG 702", course2);
@@ -41,10 +55,9 @@ public class TestPrerequesitesVerifier {
 
     @Test
     public void Should_Return_InvalidCourses_When_CourseSelectionGiven() {
-        ArrayList<Course> courseList = new ArrayList<>();
-        courseList.add(courseDatabase.get("SOFTENG 701"));
-        courseList.add(courseDatabase.get("SOFTENG 703"));
-        ArrayList<Course> rejectedCourseList = verifier.checkPrerequisites(courseList, "user123", enrollmentDatabase);
+        ArrayList<Course> courseSelectionList = new ArrayList<>();
+        courseSelectionList.add(course1);
+        ArrayList<Course> rejectedCourseList = verifier.checkPrerequisites(courseSelectionList, "user123", enrollmentDatabase);
         assertTrue(rejectedCourseList.isEmpty());
     }
 }
