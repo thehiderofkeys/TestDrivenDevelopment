ed3ec368a57a49fd187fa331f07915ee227644d0
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-05-21 15:58:28 +1200

    Add code to make test case pass

--- src/main/java/EnrollmentRequestVerifier.java
+++ src/main/java/EnrollmentRequestVerifier.java
@@ -4,14 +4,17 @@ public class EnrollmentRequestVerifier {
     public EnrollmentRequestVerifier(EnrollmentDateVerifier edv, ClashVerifier cv, PrerequisitesVerifier pv) {
     }
 
-    public EnrollmentRejection verify(String user123, ArrayList<Course> enrollList, EnrollmentDatabase mockDB) {
-        return null;
+    public EnrollmentRejection verify(String username, ArrayList<Course> enrollList, EnrollmentDatabase database) {
+        return new EnrollmentRejection(enrollList);
     }
 
     public class EnrollmentRejection{
-
+        private ArrayList<Course> enrollList;
+        private EnrollmentRejection(ArrayList<Course> enrollList){
+            this.enrollList = enrollList;
+        }
         public ArrayList<Course> getCourses() {
-            return null;
+            return enrollList;
         }
     }
 }
